94ee452aa99daf79694c85300ba1a0d9950612f9
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-01-30 23:29:45 +0200

    slider done

--- src/components/imageSlider/ImageSlider.tsx
+++ src/components/imageSlider/ImageSlider.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useRef } from 'react';
 import Link from 'next/link';
 import { Change } from '../../variablesToChange';
 
@@ -11,37 +12,27 @@ function ImageSlider() {
   const settings = {
     dots: true,
     infinite: true,
-    fade: true,
-    speed: 500,
+    speed: 700,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows:false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000
   };
+  const customSlider = useRef();
+
   return (
     <>
-      <section className='my-10 max-w-[1000px] mx-auto '>
-        <Slider {...settings}>
-          {imagesForSlider.map(
-            ({
-              id,
-              title,
-              description,
-              buttonUrl,
-              imgSrc,
-            }: {
-              id: number;
-              title: string;
-              description: string;
-              buttonUrl: string;
-              imgSrc: string;
-            }) => (
+      <section className="my-10 max-w-[1000px] mx-auto relative">
+        <Slider {...settings} ref={(slider: any) => (customSlider.current = slider)}>
+          {imagesForSlider.map(({id, title, description, buttonUrl, imgSrc}: {id: number; title: string; description: string; buttonUrl: string; imgSrc: string;}) => (
               <div>
                 <div
-                  className="border-2 border-black border-solid w-[1920px] min-h-[500px] relative bg-cover flex flex-col justify-center items-start p-10 gap-8 text-textWhiteColor"
+                  className="rounded-md min-h-[500px] relative bg-cover flex flex-col justify-center items-start p-10 gap-8 text-textWhiteColor"
                   key={id}
                   style={{ backgroundImage: `url(${imgSrc})` }}
                 >
-                  <div className="absolute top-0 bottom-0 left-0 right-0 bg-blackTransperent z-0" />
+                  <div className="absolute top-0 bottom-0 left-0 right-0 bg-blackTransperent z-0 rounded-md" />
                   <div className="flex flex-col gap-3 z-10 relative">
                     <h1 className="text-xl">{title}</h1>
                     <p>{description}</p>
@@ -58,6 +49,13 @@ function ImageSlider() {
             )
           )}
         </Slider>
+        {/* TODO переробити у компоненти й не зроз що тут хоче ts */}
+        <button className='absolute py-2 px-1 rounded-md top-1/2 right-2 bg-bgColor text-textColor' onClick={()=>{customSlider.current.slickNext()}}>
+          {'>'}
+        </button>
+        <button className='absolute py-2 px-1 rounded-md top-1/2 left-2 bg-bgColor text-textColor' onClick={()=>{customSlider.current.slickPrev()}}>
+          {'<'}
+        </button>
       </section>
     </>
   );
