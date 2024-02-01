e69c065b3b67caed8973b1264392ddf1b3a6734d
Tarold <ltdecorer@gmail.com>
2024-02-01 16:59:33 +0200

    Add map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import MapSection from '@/components/mapSection/MapSection';
+import React from 'react';
+
 export default function Home() {
-  return <main>
-  </main>;
+  return (
+    <main>
+      <MapSection></MapSection>
+    </main>
+  );
 }
