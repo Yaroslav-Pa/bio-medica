43c01d5f477544750c2268bc86836a4394434a06
Tarold <ltdecorer@gmail.com>
2024-02-03 16:48:08 +0200

    Add Cetificate Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CertificateSection from '@/components/certificateSection/CertificateSection';
 import MapSection from '@/components/mapSection/MapSection';
 import React from 'react';
 
 export default function Home() {
   return (
     <main>
+      <CertificateSection></CertificateSection>
       <MapSection></MapSection>
     </main>
   );
